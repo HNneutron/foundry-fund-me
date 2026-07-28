117dd1f8e2f4cf8244c7734630c4678b5696c167
HNneutron <neosec100@gmail.com>
2026-07-28 12:19:08 +0300

    test refactoring

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -19,7 +21,7 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public {
         console.log(fundMe.i_owner());
         console.log(msg.sender);
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
