7caa7d4c3093715191485180ff9defaf831eadc5
HNneutron <neosec100@gmail.com>
2026-08-10 05:38:51 +0300

    finalising coverages

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -85,4 +85,31 @@ contract FundMeTest is Test {
             endingOwnerBalance
         );
     }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10; //if you want to use numbers to generate addresses, then use uint160
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
 }
