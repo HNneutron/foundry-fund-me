d05bf2f8bf4cc8cdb44ac9c12043f69122ea3c48
HNneutron <neosec100@gmail.com>
2026-08-18 15:14:35 +0300

    integration tests

---
+++ script/Interactions.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+//Fund
+//Withdraw
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+    }
+}
+
+contract WithdrawFundMe is Script {}
